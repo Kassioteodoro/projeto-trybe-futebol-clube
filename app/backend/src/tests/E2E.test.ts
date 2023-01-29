f018854b2124620dbfd94c39abbcba7b18b68d93
kassio <kassioteadoro@gmail.com>
2023-01-29 00:34:19 -0300

    testes E2E: matches

--- app/backend/src/tests/E2E.test.ts
+++ app/backend/src/tests/E2E.test.ts
@@ -5,7 +5,15 @@ const { stub } = sinon;
 // @ts-ignore
 import chaiHttp = require('chai-http');
 
-import { mockToken, responseJwtValidate, responseTeamAll, responseTeamId, responseUserGet } from './mocks';
+import { mockToken,
+   responseJwtValidate,
+    responseMatchAll,
+     responseMatchEnd,
+      responseMatchInProgress,
+       responseTeamAll,
+        responseTeamId,
+         responseUserGet,
+        responseCreateMatch } from './mocks';
 import User from '../database/models/User';
 import _jwt from '../utils/jwt';
 import { app } from '../app'
@@ -13,6 +21,7 @@ import loginValidate from '../validates/Login.validate'
 
 import { Response } from 'superagent'
 import Team from '../database/models/Team';
+import Matche from '../database/models/Matche';
 
 chai.use(chaiHttp);
 
@@ -48,6 +57,7 @@ describe('testando Rodas:', function () {
 
     after(()=>{
         (User.findOne as sinon.SinonStub).restore();
+        (_jwt.generateToken as sinon.SinonStub).restore();
       })
 
       it('return status 200', function () {
@@ -138,5 +148,152 @@ describe('testando Rodas:', function () {
         expect(chaiHttpResponse2.body).to.deep.equal(responseTeamId)
       })
     })
+  })
+  describe('matches', function () {
+    let chaiHttpResponse3: Response
+
+    describe('get:/matches // All', function () {
+      before( async () => {
+        sinon
+        .stub(Matche, "findAll")
+        .resolves(responseMatchAll as Matche[]);
+
+      chaiHttpResponse3 = await chai.request(app)
+          .get('/matches')
+      })
+
+      after(()=>{
+        (Matche.findAll as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(200)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal(responseMatchAll)
+      })
+    })
+    describe('get:/matches // inPrgress', function () {
+      before( async () => {
+        sinon
+        .stub(Matche, "findAll")
+        .resolves(responseMatchInProgress as Matche[]);
+
+      chaiHttpResponse3 = await chai.request(app)
+          .get('/matches?inProgress=true')
+      })
+
+      after(()=>{
+        (Matche.findAll as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(200)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal(responseMatchInProgress)
+      })
+    })
+    describe('get:/matches // End', function () {
+      before( async () => {
+        sinon
+        .stub(Matche, "findAll")
+        .resolves(responseMatchEnd as Matche[]);
+
+      chaiHttpResponse3 = await chai.request(app)
+          .get('/matches?inProgress=false')
+      })
+
+      after(()=>{
+        (Matche.findAll as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(200)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal(responseMatchEnd)
+      })
+    })
+    describe('post:/matches', function () {
+      before( async () => {
+        sinon
+        .stub(_jwt, "verifyToken")
+        .resolves(responseJwtValidate as _jwt);
+        sinon
+        .stub(Matche, "create")
+        .resolves(responseCreateMatch as Matche);
+
+      chaiHttpResponse3 = await chai.request(app)
+          .post('/matches')
+          .auth('authorization', `${mockToken.token}`)
+          .send({
+            homeTeamId: 16,
+            awayTeamId: 8,
+            homeTeamGoals: 2,
+            awayTeamGoals: 2,
+          })
+      })
+
+      after(()=>{
+        (Matche.create as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(201)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal(responseCreateMatch)
+      })
+    })
+    describe('patch:/matches/id', function () {
+      before( async () => {
+        sinon
+        .stub(Matche, "update")
+
+      chaiHttpResponse3 = await chai.request(app)
+          .patch('/matches/20')
+          .send({
+            homeTeamGoals: 2,
+            awayTeamGoals: 2,
+          })
+      })
+
+      after(()=>{
+        (Matche.update as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(200)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal({
+          message: "atualizado"
+        })
+      })
+    })
+    describe('patch:/matches/id/finish', function () {
+      before( async () => {
+        sinon
+        .stub(Matche, "update")
+
+      chaiHttpResponse3 = await chai.request(app)
+          .patch('/matches/20/finish')
+      })
+
+      after(()=>{
+        (Matche.update as sinon.SinonStub).restore();
+      })
+
+      it('return status 200', function () {
+        expect(chaiHttpResponse3.status).to.deep.equal(200)
+      })
+      it('return objeto correto', function () {
+        expect(chaiHttpResponse3.body).to.deep.equal({
+          message: "Finished"
+        })
+      })
+    })
+
   })
 });
